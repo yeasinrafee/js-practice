29f3f3bfa362c582db5950c573b152eab07cefb7
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-01-28 21:44:03 +0600

    Odd number

--- even-odd.js
+++ even-odd.js
@@ -1,6 +1,10 @@
 // Even number
-var i = 0; 
-while(i <= 10){
+var i = 1; 
+// while(i <= 10){
+//     console.log(i);
+//     i += 2;
+// }
+
+for(; i <= 20; i+=2){
     console.log(i);
-    i += 2;
 }
