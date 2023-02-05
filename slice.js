586a2f3beab0b69ba5afa9a3240c6afcac982643
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-05 13:36:03 +0600

    js array slice method

---
+++ slice.js
@@ -0,0 +1,6 @@
+const arr = [34, 63, 23, 78, 86, 45, 97, 45, 232, 899];
+const friends = ["shakil", "kawsar", "alif", "mamun", "rimon", "dalia", "bijoy", "shafi", "mithu"];
+const partners = friends.slice(0, 4).concat(friends.slice(7));
+const arr2 = arr.slice(4, 9);
+console.log(arr2);
+console.log(partners);
